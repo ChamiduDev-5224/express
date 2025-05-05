56a8972869fa65a0723284498b36787d0ce68e04
ChamiduDev-5224 <pplchamiduravihara@gmail.com>
2025-05-05 10:20:31 +0530

    user route swagger add

--- api/user/user.router.js
+++ api/user/user.router.js
@@ -1,27 +1,47 @@
-const {create} = require('./user.controller');
+const { create } = require('./user.controller');
 const router = require('express').Router();
 
 /**
  * @swagger
  * /api/user/create:
  *   post:
- *     summary: Retrieve a list of users
+ *     summary: Create a new user
+ *     tags:
+ *       - User
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *               - email
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 example: John Doe
+ *               email:
+ *                 type: string
+ *                 example: john@example.com
  *     responses:
  *       201:
- *         description: A list of users
+ *         description: Successfully created a new user
  *         content:
  *           application/json:
  *             schema:
- *               type: array
- *               items:
- *                 type: object
- *                 properties:
- *                   id:
- *                     type: integer
- *                     example: 1
- *                   name:
- *                     type: string
- *                     example: John Doe
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: integer
+ *                   example: 1
+ *                 name:
+ *                   type: string
+ *                   example: John Doe
+ *                 email:
+ *                   type: string
+ *                   example: john@example.com
  */
+
 router.post("/create", create);
 module.exports = router;
